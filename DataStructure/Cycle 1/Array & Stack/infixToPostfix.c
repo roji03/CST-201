0e7459dabeaa0b18683cc76afedf733ded8c3970
roji varughese <123378634+roji03@users.noreply.github.com>
2023-11-17 22:29:48 +0530

    Update infixToPostfix.c

--- DataStructure/Cycle 1/Array & Stack/infixToPostfix.c
+++ DataStructure/Cycle 1/Array & Stack/infixToPostfix.c
@@ -1,120 +1,53 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-void intopost();
-int precedence(char);
-int isEmpty();
-char pop();
-void push(char);
-void print();
-
+#include <ctype.h>
 char stack[20];
-char infix[20], post[20];
 int top = -1;
-
-int main() {
-  printf("Enter exp: ");
-  fgets(infix, sizeof(infix), stdin);
-
-  intopost();
-  print();
-
-  return 0;
+void push(char x)
+{
+    stack[++top] = x;
 }
-
-void intopost()
- {
-  int i, j = 0;
-  char next, symbol;
-
-  for (i = 0; i < strlen(infix); i++) {
-    symbol = infix[i];
-
-    switch (symbol) {
-      case '(':
-        push(symbol);
-        break;
-      case ')':!= '(') {
-          post[j++] = next;
+char pop()
+{
+    if (top == -1)
+        return -1;
+    else
+        return stack[top--];
+}
+int priority(char x)
+{
+    if (x == '(')
+        return 0;
+    if (x == '+' || x == '-')
+        return 1;
+    if (x == '*' || x == '/')
+        return 2;
+    if (x == '^')
+        return 3;
+}
+void main()
+{
+    char exp[20], x;
+    printf("Enter expression: ");
+    scanf("%s", exp);
+    printf("Postfix expression: ");
+    for (int i = 0; exp[i] != '\0'; i++)
+    {
+        if (isalnum(exp[i]))
+            printf("%c", exp[i]);
+        else if (exp[i] == '(')
+            push(exp[i]);
+        else if (exp[i] == ')')
+        {
+            while ((x = pop()) != '(')
+                printf("%c", x);
         }
-        while ((next = pop()) 
-        break;
-      case '+':
-      case '-':
-      case '*':
-      case '/':
-      case '^':
-        while (!isEmpty() && precedence(stack[top]) >= precedence(symbol)) {
-          post[j++] = pop();
+        else
+        {
+            while (priority(stack[top]) >= priority(exp[i]))
+                printf("%c", pop());
+            push(exp[i]);
         }
-        push(symbol);
-        break;
-      default:
-        post[j++] = symbol;
-        break;
     }
-  }
-
-  while (!isEmpty()) {
-    post[j++] = pop();
-  }
-
-  post[j] = '\0';
-}
-
-int precedence(char symbol) {
-  switch (symbol) {
-    case '^':
-      return 3;
-    case '/':
-    case '*':
-      return 2;
-    case '+':
-    case '-':
-      return 1;
-    default:
-      return 0;
-  }
-}
-
-void print() {
-  int i = 0;
-
-  printf("ppp: ");
-  while (post[i]) {
-    printf("%c", post[i++]);
-  }
-  printf("\n");
-}
-
-void push(char c) {
-  if (top == 20) {
-    printf("overflow");
-    return;
-  }
-
-  top++;
-  stack[top] = c;
-}
-
-char pop() {
-  char c;
-
-  if (top == -1) {
-    printf("underflow");
-    exit(1);
-  }
-
-  c = stack[top];
-  top = top - 1;
-  return c;
-}
-
-int isEmpty() {
-  if (top == -1) {
-    return 1;
-  } else {
-    return 0;
-  }
+    while (top != -1)
+        printf("%c", pop());
 }
